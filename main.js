e49ca3e42f2d9f6688dee82d777e5370d720af92
Raven3 <122904758+RavenAgency@users.noreply.github.com>
2025-05-04 22:01:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Beforce “Coming Soon” page loaded');
+    // Aquí podrías añadir interacciones futuras,
+    // p.ej. un contador regresivo o enlaces sociales.
+  });
