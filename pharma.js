88493642e55ed743d81bd2cb9f5f799364215f62
Patricio Monpelat <patricio.monpelat@gmail.com>
2025-06-13 21:32:15 -0300

    .

--- pharma.js
+++ pharma.js
@@ -4,6 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
     selector: ".glightbox",
   });
 
+  const colorSelectors = document.querySelectorAll(".color-circle");
+
+  colorSelectors.forEach(function (circle) {
+    circle.addEventListener("click", function () {
+      const newImgSrc = this.getAttribute("data-img");
+
+      // Find the closest image inside the same product container
+      const productContainer = this.closest(".menu-item");
+      const productImg = productContainer.querySelector(".menu-img");
+
+      if (productImg && newImgSrc) {
+        productImg.src = newImgSrc;
+      }
+    });
+  });
+
   const swiper = new Swiper(".catalog-slider", {
     loop: true,
     autoplay: {
